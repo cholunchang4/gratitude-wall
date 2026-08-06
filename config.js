633931308370fe1c4e26b5e630742a3af6a12ae0
cholunchang4 <cholunchang4@gmail.com>
2026-08-06 17:20:45 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+// config.js
+export const firebaseConfig = {
+            apiKey: "AIzaSyAjYSBp43_8dRpGrhuTscdvbEktzogqO9o",
+            authDomain: "gratitude-wall-ad184.firebaseapp.com",
+            projectId: "gratitude-wall-ad184",
+            databaseURL:"https://gratitude-wall-ad184-default-rtdb.asia-southeast1.firebasedatabase.app/",
+            storageBucket: "gratitude-wall-ad184.firebasestorage.app",
+            messagingSenderId: "790216418295",
+            appId: "1:790216418295:web:82ec771a3cb990ef39c4f2",
+            measurementId: "G-LGMBXSE8XF"
+        };
